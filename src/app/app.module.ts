96d99be21361a2d7d2da4014464bf3287cf42e4f
nantagopalC <nantagopal3@gmail.com>
2023-03-16 19:58:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { Main2Component } from './main2/main2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    Main2Component
   ],
   imports: [
     BrowserModule,
